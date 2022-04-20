736a2dee54babe0c1075e27abf5378de46dd04f0
cyrus311 <shadow15mr@hotmail.com>
2022-04-20 12:25:51 +0300

    scraperApi added

--- index.js
+++ index.js
@@ -4,10 +4,26 @@ const request = require("request-promise");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const apiKey = "67853f854a598db46b4d87fe003b1416";
+const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Welcome to Amazon Scraper API");
+});
+
+//! GET product details
+app.get("/products/:productId", async (req, res) => {
+  const { productId } = req.params;
+  try {
+    const response = await request(
+      `${baseUrl}&url=https://www.amazon.com/dp/${productId}`
+    );
+    res.json(response);
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 app.listen(PORT, () => {
